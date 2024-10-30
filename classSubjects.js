314a00149bda5f4fb08d2398d7296880ca900749
MayRosario12 <162092946+MayRosario12@users.noreply.github.com>
2024-10-30 22:06:56 +0800

    Add files via upload

---
+++ classSubjects.js
@@ -0,0 +1,12 @@
+// Collect user information using prompt()
+let subjectTitle = prompt("DSA:");
+let classSchedule = prompt("8-9:00 AM - 11:12:30 AM, MWF:");
+let classroom = prompt("CLB:");
+let classInstructor = prompt("CO PASCUA:");
+let studentName = prompt("May rosario:");
+
+//Log the information to the console
+console.log(`${studentName} is currently enrolled in $
+{subjectTitle} with a class schedule of ${classSchedule} at room $
+{classroom}. The instructor for the subject is ${classInstructor}`);
+
